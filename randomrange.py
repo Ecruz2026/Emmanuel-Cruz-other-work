7b57a34ebd7ed7a73d408fe9aa475927ba19149f
Ecruz2026 <ecruz9@my.nl.edu>
2024-10-29 15:56:41 -0500

    Add files via upload

---
+++ randomrange.py
@@ -0,0 +1,17 @@
+
+# Emmanuel Cruz 
+
+
+#Oct 29, 2024
+
+# This program will 
+
+import random
+
+rand_nums = []
+
+for _ in range (10):
+    num = random.randrange (25, 36)
+    rand_nums.append(num)
+    print (num)
+    print ("List of random numbers:", rand_nums)
